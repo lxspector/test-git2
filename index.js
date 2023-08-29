89e156c25c46feedebb4c51f1c7a542eaf3ae02c
lxspector <lxspector@gmail.com>
2023-08-30 00:30:03 +0200

    main upd

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import { error, log } from 'console'
 
 log('Hello world')
+
+const age = 20
+
+if (age >= 19) {
+  console.log('Совершеннолетний')
+} else {
+  console.log(`Не совершеннолетний`)
+}
